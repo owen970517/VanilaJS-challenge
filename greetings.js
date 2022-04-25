34a040d946a66f0892268e07e536db84496abf8f
Owen <tamia123123@naver.com>
2022-04-25 15:15:23 +0900

    update

--- greetings.js
+++ greetings.js
@@ -2,6 +2,7 @@ const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
 const greeting  = document.querySelector("#greeting");
 const logoutBtn = document.querySelector("#logout-btn");
+const list =  document.getElementById("todo-list");
 // ()가 존재하면 브라우저가 바로 실행시킴 
 function onLoginSubmit(event) {
     // 브라우저의 기본 동작을 막아줌 
@@ -20,6 +21,7 @@ function paintGreeting(username) {
     greeting.innerText = `Welcome ${username}`;
     greeting.classList.remove("hidden");
     logoutBtn.classList.remove("hidden");
+    list.classList.remove('hidden');
 }
 
 //  "이름"인 Key의 Value 값을 saveUsername에 저장 
@@ -29,6 +31,7 @@ if(saveUsername === null) {
     //loginform을 보여주고 
     loginForm.classList.remove("hidden");
     loginForm.addEventListener("submit" , onLoginSubmit);
+    list.classList.add('hidden');
 } else {
     paintGreeting(saveUsername);
 
@@ -41,5 +44,6 @@ function LogOut() {
     loginForm.classList.remove("hidden");
     loginInput.value = "";
     loginForm.addEventListener("submit" , onLoginSubmit);
+    list.classList.add('hidden');
 }
 logoutBtn.addEventListener("click" , LogOut);
